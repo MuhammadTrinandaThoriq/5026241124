d067354606f6dfc1c30458d307012e5e2a1f7783
MuhammadTrinandaThoriq <trestblox@gmail.com>
2026-04-25 19:34:34 +0700

    tugas pertemuan 7

--- routes/web.php
+++ routes/web.php
@@ -21,3 +21,39 @@ Route::get('pert5', function () {
 
 Route::get('dosen', [DosenController::class, 'index']);
 Route::get('biodata', [DosenController::class, 'biodata']);
+
+Route::get('pert1', function () {
+    return view('pertemuan1');
+});
+
+Route::get('pert2file1', function () {
+    return view('pertemuan2news');
+});
+
+Route::get('pert2file2', function () {
+    return view('pertemuan2news1');
+});
+
+Route::get('pert3file1', function () {
+    return view('pertemuan3contoh');
+});
+
+Route::get('pert3file2', function () {
+    return view('pertemuan3responsive');
+});
+
+Route::get('pert3file3', function () {
+    return view('pertemuan3template');
+});
+
+Route::get('pert4', function () {
+    return view('pertemuan4');
+});
+
+Route::get('pert5file2', function () {
+    return view('pertemuan5index');
+});
+
+Route::get('pert5file3', function () {
+    return view('pertemuan5linktree');
+});
